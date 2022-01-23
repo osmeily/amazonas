1f5d2ee17b61e4c2e8e66101b5504f2dc84e239e
Osmeily Pernia <osmeilypernia@gmail.com>
2022-01-23 15:24:55 -0500

    addProduct page redesigned

--- src/components/addProduct/AddProduct.js
+++ src/components/addProduct/AddProduct.js
@@ -4,10 +4,17 @@ import {useFormik} from "formik"
 import { fileUpload } from '../../helpers/fileUpload'
 import { useDispatch } from 'react-redux'
 import { addProductsAsync } from '../../actions/actionProducts'
+import paso1 from "../../resources/paso1.png"
+import paso2 from "../../resources/paso2.png"
+import paso3 from "../../resources/paso3.png"
+import logo from "../../resources/logo-amazon (1).png"
+import { useNavigate } from 'react-router-dom'
+
 
 const AddProduct = () => {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const formik = useFormik({
         initialValues:{
@@ -102,36 +109,43 @@ const AddProduct = () => {
     }
 
     return (
-        <div>
+        <div className='form-div'>
+            <img onClick={()=> navigate("/")} className='addLogo' src={logo} alt=""/>
             <form className='form-add' onSubmit={formik.handleSubmit}>
-            <div>
-                <input name="imgUrl" id='imgUrl' style={{display:"none"}} type='button' onChange={handleFileChangeimgUrl}/>
-                <button onClick={handleClickImgUrl}>Imagen card</button>
-                <input name="imagen1" id='imagen1' style={{display:"none"}} type='button' onChange={handleFileChange1}/>
-                <button onClick={handleClickPic1}>Imagen 1</button>
-                <input  name="imagen2" id='imagen2' style={{display:"none"}} type="button" onChange={handleFileChange2}/>
-                <button onClick={handleClickPic2}>Imagen 2</button>
-                <input  name="imagen3" id='imagen3' style={{display:"none"}} type="button" onChange={handleFileChange3}/>
-                <button onClick={handleClickPic3}>Imagen 3</button>
-            </div>
-            <div>
-                <label>Nombre del producto</label>
-                <input name='productName' onChange={formik.handleChange}/>
-            </div>
-            <div>
-                <label>Categoría</label>
-                <input name='category' onChange={formik.handleChange} />
-            </div>
-            <div>
-                <label>Descripción del producto</label>
-                <input name='productDetail' onChange={formik.handleChange}/>
-            </div>
-            <div>
-                <label>Precio</label>
-                <input name='productPrice' onChange={formik.handleChange} />
-            </div>
-            
-            <button type='submit'>Agregar producto</button>
+                <p className='steps'><img style={{width:"80px"}} src={paso1} alt=''/> Agrega todas las imágenes, empezando por la que mostrarás en lista</p>
+                <div className='image'>
+                    <input name="imgUrl" id='imgUrl' style={{display:"none"}} type='button' onChange={handleFileChangeimgUrl}/>
+                    <button onClick={handleClickImgUrl}>Imagen card</button>
+                    <input name="imagen1" id='imagen1' style={{display:"none"}} type='button' onChange={handleFileChange1}/>
+                    <button onClick={handleClickPic1}>Imagen 1</button>
+                    <input  name="imagen2" id='imagen2' style={{display:"none"}} type="button" onChange={handleFileChange2}/>
+                    <button onClick={handleClickPic2}>Imagen 2</button>
+                    <input  name="imagen3" id='imagen3' style={{display:"none"}} type="button" onChange={handleFileChange3}/>
+                    <button onClick={handleClickPic3}>Imagen 3</button>
+                </div>
+                <hr/>
+                <p className='steps'><img style={{width:"80px"}} src={paso2} alt=''/> Llena los campos con la información del producto</p>
+                <div className='input-info'>
+                    <div>
+                        <label>Nombre del producto :</label>
+                        <input name='productName' onChange={formik.handleChange}/>
+                    </div>
+                    <div>
+                        <label>Categoría :</label>
+                        <input name='category' onChange={formik.handleChange} />
+                    </div>
+                    <div>
+                        <label>Descripción del producto :</label>
+                        <input name='productDetail' onChange={formik.handleChange}/>
+                    </div>
+                    <div>
+                        <label>Precio :</label>
+                        <input name='productPrice' onChange={formik.handleChange} />
+                    </div>
+                </div>
+                <hr/>
+                <p className='steps'><img style={{width:"80px"}} src={paso3} alt=''/> Tu producto está listo para ser agregado</p>
+                <button className='add-btn' type='submit'>Agregar producto</button>
         </form>
         </div>
     )
